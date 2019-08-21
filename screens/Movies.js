2dc359da869b99f7751acf4a1b49a35a376450b3
Inmobi Korea <taki1984@naver.com>
2019-08-21 17:20:50 +0900

    Networking Test

--- screens/Movies.js
+++ screens/Movies.js
@@ -1,21 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Text } from "react-native";
 import styled from "styled-components";
 import { TouchableOpacity } from "react-native-gesture-handler";
+import useAxios from "../useAxios";
 
 const Container = styled.View`
-    flex:1;
-    justify-content: center;
-    align-items: center;
+  flex: 1;
+  justify-content: center;
+  align-items: center;
 `;
 
-export default ({ navigation }) => (
-  <>
-    <Container>
-      <Text>Movies</Text>
-      <TouchableOpacity onPress={() => navigation.navigate('Detail')}>
-        <Text>Go to detail</Text>
-      </TouchableOpacity>
-    </Container>
-  </>
-);
+export default ({ navigation }) => {
+  const { loading, data, refetch } = useAxios({
+    url: "https://yts.am/api/v2/list_movies.json"
+  });
+
+  return (
+    <>
+      <Container>
+        <Text>{data && data.status}</Text>
+        <Text>{loading && "Loading"}</Text>
+        <TouchableOpacity onPress={() => navigation.navigate("Detail")}>
+          <Text>Hello</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={refetch}>
+          <Text>Refetch</Text>
+        </TouchableOpacity>
+      </Container>
+    </>
+  );
+};
